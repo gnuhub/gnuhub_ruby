01a9babc2afeb04c59f88d377413ea85e6c03abf
gnuhub <gnuhub@gmail.com>
2014-03-21 15:42:53 +0800

    add ruby c file

---
+++ myext.c
@@ -0,0 +1,13 @@
+//
+//  myext.c
+//  gnuhub_ruby
+//
+//  Created by gnuhub on 14-3-21.
+//  Copyright (c) 2014年 com.gnuhub. All rights reserved.
+//
+
+#include "ruby.h"
+
+void Init_myext() {
+    printf("Hello Ruby from C!\n");
+}
