c3857b6cb962443db8c72e1532ad1bed765b6e64
gnuhub <gnuhub@gmail.com>
2014-03-21 20:42:32 +0800

    5 is T_STRING #2

--- myext.c
+++ myext.c
@@ -12,4 +12,7 @@ void Init_myext() {
     VALUE v1;
     printf("%d\n",sizeof(v1));
     printf("Hello Ruby from C!\n");
+    VALUE str;
+    str = rb_str_new(0,0);    /* creates a Ruby string (its struct is RString) */
+    printf("%d\n",TYPE(str));
 }
